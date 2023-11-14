e1dfdb8b8bb75697672423bad9f59fe15acef6f4
vardanm1993 <vardanm1993@gmail.com>
2023-11-14 09:39:13 +0000

    Added Session and changed Container

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Core\Request;
+
 class MainController
 {
+    public function __construct(protected Request $request)
+    {
+
+    }
     public function index()
     {
-        echo "This is index page";
+        dd($_SESSION['resolved']) ;
     }
 }
