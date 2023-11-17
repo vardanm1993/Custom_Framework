59a946e108b0b50ace203c9377e99899a90789da
vardanm1993 <vardanm1993@gmail.com>
2023-11-17 12:04:09 +0000

    Refactoring MainController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use Core\Request;
 
-class MainController
+class TestController
 {
     public function __construct(protected Request $request)
     {
 
     }
+
     public function index()
     {
         echo "This is home page";
     }
+
     public function show($id)
     {
         echo $id;
