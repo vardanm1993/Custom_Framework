71f6f46a4b83be8351b527af8487dc3ad48145fa
vardanm1993 <vardanm1993@gmail.com>
2023-11-21 14:20:43 +0000

    Added Response to DI Container

--- config/app.php
+++ config/app.php
@@ -2,7 +2,9 @@
 
 use Core\App;
 use Core\Request;
+use Core\Response;
 use Core\View;
 
 App::$container->bind(Request::class,fn() => new Request($_SERVER,$_GET,$_POST,$_COOKIE,$_FILES));
+App::$container->bind(Response::class,fn() => new Response());
 App::$container->bind(View::class,fn() => new View());
