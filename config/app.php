c42cb8e23544ca69ccac2e56d1489abd013b1402
vardanm1993 <vardanm1993@gmail.com>
2023-11-21 13:38:30 +0000

    Creating View in Container

--- config/app.php
+++ config/app.php
@@ -2,5 +2,7 @@
 
 use Core\App;
 use Core\Request;
+use Core\View;
 
 App::$container->bind(Request::class,fn() => new Request($_SERVER,$_GET,$_POST,$_COOKIE,$_FILES));
+App::$container->bind(View::class,fn() => new View());
